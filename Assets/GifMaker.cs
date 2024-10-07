e36b15d46dc76e9ce74fec1a244795a40a9966ca
100thCoin <onehundredthcoin@gmail.com>
2024-10-06 20:33:59 -0400

    The pause menu "return to title" buttons work now
    The pause menu "return to title" buttons work now

---
+++ Assets/GifMaker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GifMaker : MonoBehaviour {
+	public int count = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public RenderTexture RT;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Texture2D RTRender = new Texture2D(512,16,TextureFormat.RGBA32,false);
+		RenderTexture.active = RT;
+
+		RTRender.ReadPixels (new Rect (0, 0, 512, 16), 0, 0);
+		File.WriteAllBytes ("C:\\Users\\100th_Coin\\Documents\\LudumDare56\\birdgif\\Image_" + count + ".png",RTRender.EncodeToPNG());
+		count++;
+	}
+}
--- Assets/Scripts/QuitToTitleButton.cs
+++ Assets/Scripts/QuitToTitleButton.cs
@@ -21,9 +21,7 @@ public class QuitToTitleButton : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Super.Dataholder.LockTitle) {
-			return;
-		}
+
 		if (hoverGrace > 0) {
 			SR.sprite = Hover;
 			if (Input.GetKeyDown (KeyCode.Mouse0)) {
